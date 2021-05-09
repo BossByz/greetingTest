b70d42d10f9191e878f8d4939a5e8473d39fd101
Byron <byronrvarty44@gmail.com>
2021-05-09 17:09:26 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='greetingTest',
+    version='1.0',
+    description='Testing installation of Package',
+    url='https://github.com/BossByz/greetingTest',
+    author='BossByz',
+    author_email='sumtingwong1234321@gmail.com',
+    packages=['greetingTest'],
+)
